7dac249364e9b89178d46c783750d18aa262d55a
Enrique Sanz <enriquesanz@gmail.com>
2018-03-07 17:07:24 +0100

    clean up

--- js/servicepedia.js
+++ js/servicepedia.js
@@ -2,9 +2,7 @@ var summaryURL = 'https://simpatico.hi-iberia.es:4570/cpd/api/diagram/summary/li
 var availableTags = [];
 $.getJSON( summaryURL, function( data ) {
     $.each(data,function(index,value){
-      //var procedure = {'id': data[index].phases[0].eServiceId, 'name' : data[index].name };
       availableTags.push(data[index].phases[0].eServiceId + "-" + data[index].name);
-      //availableTags.push(procedure);
     })
   });
 
@@ -23,18 +21,10 @@ $( function() {
     });
   } );
 
-  $('.input').keypress(function (e) {
-    if (e.which == 13) {
-      console.log("Betis");
-      return false;    //<---- Add this line
-    }
-  });
+function searchBar ()
+{
+  console.log("Yendo");
+  var goTo = $( "#tags" ).val().split('-');
+  window.location.href ='procedure.html?'+goTo[0];
 
-
-  function searchBar ()
-  {
-    console.log("Yendo");
-    var goTo = $( "#tags" ).val().split('-');
-    window.location.href ='procedure.html?'+goTo[0];
-
-  }
+}
