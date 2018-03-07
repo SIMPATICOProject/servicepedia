318477b4e63bf8d90b64e59c6cdab42f88b7e1c6
Enrique Sanz <enriquesanz@gmail.com>
2018-03-07 14:22:15 +0100

    Fixes search

--- js/servicepedia.js
+++ js/servicepedia.js
@@ -1,7 +1,11 @@
 var summaryURL = 'https://simpatico.hi-iberia.es:4570/cpd/api/diagram/summary/list';
 var availableTags = [];
 $.getJSON( summaryURL, function( data ) {
-    $.each(data,function(index,value){availableTags.push(data[index].name);})
+    $.each(data,function(index,value){
+      //var procedure = {'id': data[index].phases[0].eServiceId, 'name' : data[index].name };
+      availableTags.push(data[index].phases[0].eServiceId + "-" + data[index].name);
+      //availableTags.push(procedure);
+    })
   });
 
 $( function() {
@@ -10,6 +14,19 @@ $( function() {
       classes: {
         "ui-autocomplete": "highlight"
       },
+      list: {
+        match: {
+          enabled: true
+        }
+      },
       appendTo: "#searchBar"
     });
   } );
+
+  function searchBar ()
+  {
+    console.log("Yendo");
+    var goTo = $( "#tags" ).val().split('-');
+    window.location.href ='procedure.html?'+goTo[0];
+
+  }
