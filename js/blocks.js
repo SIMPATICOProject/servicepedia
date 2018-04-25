e4663f38d6b331d2d5f9d5fbb54f6c4eb815af16
Enrique Sanz <enriquesanz@gmail.com>
2018-04-25 11:46:15 +0200

    Changed blocks for steps

--- js/blocks.js
+++ js/blocks.js
@@ -10,14 +10,25 @@ function fillData(blockData)
 
     $('#procedure_title').text(blockData[0].model.description.it);
 
-    $.each(blockData[0].model.blocks,function(index,value){
+    $.each(blockData[0].model.workflow,function(index,value){
 
         var blockHTMl = '<div class="col-xs-3 bs-wizard-step disabled">' + 
-            '<div class="text-center bs-wizard-stepnum">'+ blockData[0].model.blocks[index].id +'</div>' + 
+            '<div class="text-center bs-wizard-stepnum">'+ blockData[0].model.workflow[index].id + ' ' + blockData[0].model.workflow[index].name.it + '</div>' + 
             '<div class="progress"><div class="progress-bar"></div></div>' + 
             '<a href="#" class="bs-wizard-dot"></a>' +
-            '<div class="bs-wizard-info text-center">'+ blockData[0].model.blocks[index].description.it + '</div>' +
+            '<div class="bs-wizard-info text-center">'+ blockData[0].model.workflow[index].description.it + '</div>' +
         '</div>' ;
+
+// var blockHTMl = '<div class="3ap">'+
+//         '<div class="col-3">' + 
+//             '<div class="card card-block">'+ blockData[0].model.blocks[index].id +'</div>' + 
+//             blockData[0].model.blocks[index].description.it
+//         '</div>';
+
+// var blockHTMl = '<div class="col-xs-3">' + 
+//         blockData[0].model.blocks[index].id +
+//         '<div class="well">'+blockData[0].model.blocks[index].description.it+'</div>' +
+//     '</div>';
 
         $('#blocks_container').append(blockHTMl);
       })
