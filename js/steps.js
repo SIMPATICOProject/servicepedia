e72b97fc32d5a16e59f7704afd014258462962ba
Enrique Sanz <enriquesanz@gmail.com>
2018-04-25 13:35:20 +0200

    Changed blocks for steps

--- js/steps.js
+++ js/steps.js
@@ -13,24 +13,13 @@ function fillData(blockData)
     $.each(blockData[0].model.workflow,function(index,value){
 
         var blockHTMl = '<div class="col-xs-3 bs-wizard-step disabled">' + 
-            '<div class="text-center bs-wizard-stepnum">'+ blockData[0].model.workflow[index].id + ' ' + blockData[0].model.workflow[index].name.it + '</div>' + 
+            '<div class="text-center bs-wizard-stepnum">'+ (index + 1) + '-' + blockData[0].model.workflow[index].name.it + '</div>' + 
             '<div class="progress"><div class="progress-bar"></div></div>' + 
             '<a href="#" class="bs-wizard-dot"></a>' +
             '<div class="bs-wizard-info text-center">'+ blockData[0].model.workflow[index].description.it + '</div>' +
         '</div>' ;
 
-// var blockHTMl = '<div class="3ap">'+
-//         '<div class="col-3">' + 
-//             '<div class="card card-block">'+ blockData[0].model.blocks[index].id +'</div>' + 
-//             blockData[0].model.blocks[index].description.it
-//         '</div>';
-
-// var blockHTMl = '<div class="col-xs-3">' + 
-//         blockData[0].model.blocks[index].id +
-//         '<div class="well">'+blockData[0].model.blocks[index].description.it+'</div>' +
-//     '</div>';
-
-        $('#blocks_container').append(blockHTMl);
+        $('#steps_container').append(blockHTMl);
       })
 
     
